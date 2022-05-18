eb544881008f4ac0d7b5368fb0e654cb05ab5992
Rishika1416 <68844357+Rishika1416@users.noreply.github.com>
2022-05-18 13:20:16 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -12,8 +12,6 @@ class SendEmail:
     def __init__(self, to_):
         self.gmail_user = config('GMAIL_ID')
         self.gmail_password = config('GMAIL_PASSWORD')
-
-        #to_=''vaishrishika@gmail.com''
         self.to_=to_
 
     #for appple mail make changes here
@@ -23,25 +21,10 @@ class SendEmail:
         msgRoot['To'] = self.to_
         msgRoot['Subject'] = subject
         message =MIMEMultipart('alternative')
-        #message.attach(MIMEText('<b>This is the <i>HTML</i> content of this email</b> it contains an image.<br><img src="cid:image1"><br>This is a text below the image<br>',_subtype='html'))
         message.attach(MIMEText(msg_html,_subtype='html'))
         msgRoot.attach(message)
-
-        #adding attachment to mail  'cats_img/images2.jpg'
-        if file==None:
-            return msgRoot
-        else:
-            with open(file, 'rb') as f:
-                image=MIMEImage(f.read(),_subtype="jpg")
-                if msgRoot.is_multipart():
-                    print("yes_mail is multiport")
-                image.add_header('Content-ID', '<image1>')
-                msgRoot.attach(image)
-                f.close()
-                return msgRoot
     
     def send_mail(self,msgRoot):
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            #text= message.as_string()
             smtp.login(self.gmail_user, self.gmail_password) #Login to SMTP server
             smtp.send_message(msgRoot, self.gmail_user,self.to_)
