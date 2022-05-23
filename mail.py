4938a2fa6edae057cdb790b2a454c6e7dbb71924
Rishika1416 <68844357+Rishika1416@users.noreply.github.com>
2022-05-23 12:10:39 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -9,11 +9,11 @@ from tkinter import simpledialog
 from RPA.Robocorp.Vault import Vault
 #application_window = tk.Tk()
 
-#mail= simpledialog.askstring("Input", "MailID?",parent=application_window)
-#password= simpledialog.askstring("Input", "Password", parent=application_window)
-secret = Vault().get_secret("Config")
-mail = secret['sender_email']
-password = secret['sender_password']
+mail= simpledialog.askstring("Input", "MailID?",parent=application_window)
+password= simpledialog.askstring("Input", "Password", parent=application_window)
+# secret = Vault().get_secret("Config")
+# mail = secret['sender_email']
+# password = secret['sender_password']
 class SendEmail:
     #loading login credentials 
     def __init__(self):
